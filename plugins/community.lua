d0d384df9689749c05ef30a6e8f08459a7de14e4
Evgeny Krutko <e.s.krutko@gmail.com>
2023-08-03 23:19:45 +0300

    languages and mini-move

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,18 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+
+  -- languages
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.cmake" },
+  { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.rust" },
+
+  -- move selections
+  { import = "astrocommunity.motion.mini-move" },
 }
